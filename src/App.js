13c032c79be809f1b40ae5762990cd387b9cb039
matiaspompilio <matiaspompilio97@gmail.com>
2021-11-04 12:04:51 -0300

    eslint changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import logo from './logo.svg';
-import './App.css';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>We now have Auth!</h1>
-      </header>
-      <AmplifySignOut />
-    </div>
+<div className="App">
+  <header>
+    <img src={logo} className='App-logo' alt="logo" />
+    <h1>We now have Auth!</h1>
+  </header>
+  <AmplifySignOut />
+</div>
   );
 }
 
